f1436bb9b94cc05385290fb48a8231318d6dfd79
green-nisaba <basilandsalt@gmail.com>
2023-01-09 17:34:21 +0000

    Add code to increment dcore and incorrect anser tallY"

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,9 +51,15 @@ function calculateCorrectAnswer() {
 }
 function incrementScore() {
 
-}
-function incrementWrongAnswer() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
+}
+
+function incrementWrongAnswer() {
+ let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+    
 }
 function displayAdditionQuestion (operand1, operand2) {
 document.getElementById("operand1").textContent = operand1;
